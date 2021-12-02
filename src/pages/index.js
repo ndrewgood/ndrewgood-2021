0226ec44e9d393abed4ccac4d6b96073058b857e
Andrew Goodridge <gandrew888@gmail.com>
2021-12-02 12:17:22 -0500

    more tests

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,14 +65,19 @@ const Website = ({year, title, stack, link, color, order}) => {
     "bg-purple",
     "bg-yellow"
   ]
+  if (typeof window !== "undefined") {
+    return(
+      <a className={"website" + color} href={link} onMouseEnter={() => {setHoverStatus(true); console.log(backgroundColor)}} onMouseLeave={() => {setHoverStatus(false); console.log(backgroundColor)}}>
+        <span className={"title " + bgColors[order]}>{title}</span>
+        <span className="year">{backgroundColor}, {order}</span>
+        <span className={"stack " + bgColors[order]}>{stack}</span>
+      </a>
+    )
+} else {
+    return null
+}
 
-  return(
-    <a className="website" href={link} onMouseEnter={() => {setHoverStatus(true); console.log(backgroundColor)}} onMouseLeave={() => {setHoverStatus(false); console.log(backgroundColor)}}>
-      <span className={"title " + bgColors[order]}>{title}</span>
-      <span className="year">{backgroundColor}, {order}</span>
-      <span className={"stack " + bgColors[order]}>{stack}</span>
-    </a>
-  )
+
 }
 
 const Footer = () => {
